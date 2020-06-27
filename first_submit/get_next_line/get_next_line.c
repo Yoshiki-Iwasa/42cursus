93237029cd295001bf317008d577433edc162b96
Yoshiki Iwasa <yiwasa@c1r18s1.42tokyo.jp>
2020-06-27 18:00:48 +0900

    get-next-lineの初め

--- first_submit/get_next_line/get_next_line.c
+++ first_submit/get_next_line/get_next_line.c
@@ -6,37 +6,87 @@
 /*   By: yiwasa <yiwasa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 11:30:25 by yiwasa            #+#    #+#             */
-/*   Updated: 2020/06/26 13:30:11 by yiwasa           ###   ########.fr       */
+/*   Updated: 2020/06/27 17:57:55 by yiwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 100000000;
+//////これ、ファイルの中にヌルが入ってた場合はどうするんだろか。。。。。
+//////mallocがおきて、その内容が破棄されるたびに、freeしないと
 
 int get_next_line(int fd, char **line)
 {
-	char buf[100000000]; 
-	while(!read(fd,&buf,1))
+	char buf[BUFFER_SIZE + 1];
+	static char *s;
+	char *tmp;
+	char *tmp2;
+	int len;
+	int rv;
+	int i;
+
+	if (fd < 0 || line == NULL)
+		return (-1);
+	while((rv = read(fd,&buf,BUFFER_SIZE)) > 0)
 	{
-		
+		i++;
+		buf[rv] = '\0';
+		if(s == NULL)
+			s = ft_strdup(buf);
+		else
+		{
+			tmp = ft_strjoin(s,buf);
+			free(s);
+			s = tmp;
+		}
+		if(ft_strchr(s,'\n'))
+			break;	
 	}
+	if(rv == 0 && s == NULL)
+	{
+		free(s);
+		return (0);
+	}
+	else if(rv < 0)
+		return (-1);
+	else
+	{
+		len = 0;
+		while(s[len] != '\n' && s[len] != '\0')
+		{
+			write(1,&s[len],1);
+			len++;
+		}
+		if(s[len] == '\0')
+		{
+			*line = ft_strdup(s);
+			s = NULL;
+		}
+		else
+		{	
+			*line = ft_substr(s,0,len);
+			tmp2 = ft_strdup(&s[len + 1]);
+			free(s);
+			s = tmp2;	
+		}
+		return (1);
+	}	
 }
 
-
-///とりあえず、一行ずつ読みこむようにしとくか
-//baffaniitigyouzutuyomikomaseteriku
-//難しいプログラムを描くときは、
-
+//ファイル一個、読み込むのはできた。
+//ファイルの内容によって例外処理しよう。
+// 改行だけ -> 1、何にもなし-> 0、非表示もじ、
 int main()
 {
 	int fd;
 	char *line;
-
-	fd = open("text.txt",O_RDONLY);
+	char buf[BUFFER_SIZE];
+	line = malloc(BUFFER_SIZE);
+	fd = open("text.txt",O_RDONLY);	
+	ft_putnbr_fd(get_next_line(fd,&line),1);
 	while(get_next_line(fd,&line))
 	{
-		printf("%s\n",line);
+		ft_putstr_fd(line,fd);
 	}
 	return (0);
 }
--- first_submit/get_next_line/get_next_line.h
+++ first_submit/get_next_line/get_next_line.h
@@ -5,7 +5,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-#define BUFF_SIZE 1
+#include <limits.h>
+#define BUFFER_SIZE 100000
 int get_next_line(int fd, char **line);
-
+char	*ft_strdup(const char *src);
+size_t		ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int ch);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
 #endif
